db27c7d4b1b6c4388ad07f8f5f37016f20f2e536
BeatryzEmerich <biaemerich22@gmail.com>
2023-09-02 14:20:13 -0300

    subindo o inicio do projeto
    commitando a estrutura básica do projeto

---
+++ src/main/java/bia/biaspringsecurityjwt/BiaSpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package bia.biaspringsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiaSpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiaSpringSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bia/biaspringsecurityjwt/BiaSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package bia.biaspringsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiaSpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
